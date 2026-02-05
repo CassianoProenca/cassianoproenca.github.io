2f00ab8294c1ebd987dd22c73826b86be961877e
CassianoProenca <cassianomeloprofissional@gmail.com>
2026-02-05 15:19:28 -0300

    update base path in Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/cassianoproenca.github.io/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
